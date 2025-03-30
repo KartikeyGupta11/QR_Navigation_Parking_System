6460f01a5ae402bb085b4b42b31ebf6dc4e41213
Kartikey <g.kartikey.1104@gmail.com>
2025-03-30 13:25:03 +0530

    little change

--- frontend/src/ServerURL.jsx
+++ frontend/src/ServerURL.jsx
@@ -1,3 +1,3 @@
-// const  SERVER_URL="https://smart-nb4h.onrender.com";
-const SERVER_URL="http://localhost:3000";
+const  SERVER_URL="https://smart-nb4h.onrender.com";
+// const SERVER_URL="http://localhost:3000";
 export default SERVER_URL;
